2743d9302275f463fff97d5c4f9cfc170cc98195
Sacha Guilbert <sachaguilbert@gmail.com>
2023-06-19 13:42:29 +0200

    More planets

--- Inc/entities.h
+++ Inc/entities.h
@@ -46,7 +46,7 @@ typedef struct{
 }planet_t;
 //PLANET
 void initPlanet(planet_t *planet, int32_t x, int32_t y, uint8_t style);
-void planetRandom(planet_t *pla);
+void planetRandom(planet_t *pla,uint8_t arrsize);
 void playerCollision(player_t *p, astroid_t *a);
 // PLAYER
 player_t initPlayer(int32_t x, int32_t y);
